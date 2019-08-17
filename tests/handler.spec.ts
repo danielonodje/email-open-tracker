7c4319de721bfb0a182744ee48b877a9559963a6
Daniel Onodje <danielonodje@gmail.com>
2019-08-17 23:19:48 +0200

    write event handling controller test event handling controller

---
+++ tests/handler.spec.ts
@@ -0,0 +1,90 @@
+import { strictEqual, deepEqual } from 'assert';
+import { DynamoDB, SNS } from 'aws-sdk';
+import { notAcceptable, badImplementation } from '@hapi/boom';
+import { handleEvent } from '../src/handler';
+
+import {
+	createValidPayload,
+	createPayloadWithInvalidSignature,
+	createDynamoDBMock,
+	createSNSMock,
+	createFailingDynamoDBMock
+} from './testUtils';
+
+describe('EmailEvent handling', function() {
+	describe('handleEvent function', function() {
+		it('should return a notAcceptableError(406) with a wrong signature', async function() {
+			const payload = createPayloadWithInvalidSignature();
+
+			const callback = jest.fn();
+			const dynamoDBMock = createDynamoDBMock();
+			const SNSMock = createSNSMock();
+
+			await handleEvent(
+				payload,
+				// SNSMock and jest.Mocked<DynamoDB> do not overlap so we'll first
+				// cast to unknown before casting to jest.Mocked<DynamoDB>
+				(dynamoDBMock as unknown) as jest.Mocked<DynamoDB>,
+				(SNSMock as unknown) as jest.Mocked<SNS>,
+				callback
+			);
+
+			const response = callback.mock.calls[0][0];
+
+			deepEqual(response, notAcceptable());
+		});
+
+		it('should return a badImplementation(500) when it fails to save', async function() {
+			const payload = createValidPayload();
+
+			const callback = jest.fn();
+			const dynamoDBMock = createFailingDynamoDBMock();
+			const SNSMock = createSNSMock();
+
+			await handleEvent(
+				payload,
+				(dynamoDBMock as unknown) as jest.Mocked<DynamoDB>,
+				(SNSMock as unknown) as jest.Mocked<SNS>,
+				callback
+			);
+
+			const response = callback.mock.calls[0][0];
+
+			deepEqual(response, badImplementation());
+		});
+
+		it('should return a 200 statusCode when no errors occur', async function() {
+			const payload = createValidPayload();
+
+			const callback = jest.fn();
+			const dynamoDBMock = createDynamoDBMock();
+			const SNSMock = createSNSMock();
+
+			await handleEvent(
+				payload,
+				(dynamoDBMock as unknown) as jest.Mocked<DynamoDB>,
+				(SNSMock as unknown) as jest.Mocked<SNS>,
+				callback
+			);
+
+			const response = callback.mock.calls[0][0];
+
+			deepEqual(response, { statusCode: 200 });
+		});
+
+		it('should attempt to push the event to the SNS service', async function() {
+			const payload = createValidPayload();
+
+			const callback = jest.fn();
+			const dynamoDBMock = (createDynamoDBMock() as unknown) as jest.Mocked<
+				DynamoDB
+			>;
+			const SNSMock = (createSNSMock() as unknown) as jest.Mocked<SNS>;
+			const publishItemSpy = SNSMock.publish;
+
+			await handleEvent(payload, dynamoDBMock, SNSMock, callback);
+
+			strictEqual(publishItemSpy.mock.calls.length, 1);
+		});
+	});
+});
