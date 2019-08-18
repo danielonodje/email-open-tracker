7ff6e991451b0ecaeb61ed1a45f92a89c88a224d
Daniel Onodje <danielonodje@gmail.com>
2019-08-18 18:09:28 +0200

    remove require env variables

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import { EmailEvent } from 'EmailEvent';
 import { DynamoDB, SNS, config as AWSConfig } from 'aws-sdk';
-import requireEnv from 'require-environment-variables';
 import { handleEvent } from './handler';
 
-const expectedEnvVariables = [
-	'HMAC_SEED',
-	'SNS_TOPIC_ARN',
-	'DYNAMO_DB_TABLE_NAME',
-	'AWS_ACCESS_KEY_ID',
-	'AWS_SECRET_ACCESS_KEY',
-	'AWS_REGION'
-];
+function verifyEnvVariables() {
+	const expectedEnvVariables = [
+		'HMAC_SEED',
+		'SNS_TOPIC_ARN',
+		'DYNAMO_DB_TABLE_NAME',
+		'AWS_ACCESS_KEY_ID',
+		'AWS_SECRET_ACCESS_KEY',
+		'AWS_REGION'
+	];
+
+	const missingEnvVariables = expectedEnvVariables.filter(
+		k => process.env[k] === undefined
+	);
+
+	if (missingEnvVariables.length > 0) {
+		console.error(
+			`The following required env variables are missing: ${missingEnvVariables.join(
+				','
+			)}`
+		);
+	}
+}
 
 export const handler = async (event: any, _context: {}, callback: Function) => {
-	requireEnv(expectedEnvVariables);
+	verifyEnvVariables();
 
 	const db = new DynamoDB({ apiVersion: '2012-08-10' });
 	var SNSClient = new SNS({ apiVersion: '2012-08-10' });
